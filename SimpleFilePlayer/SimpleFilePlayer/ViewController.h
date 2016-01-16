b35e35d84884712d4087d16cadd5046bd9b5f8b7
Syed Haris Ali <syedhali07@gmail.com>
2016-01-15 18:07:19 -0600

    Adding in example

--- SimpleFilePlayer/SimpleFilePlayer/ViewController.h
+++ SimpleFilePlayer/SimpleFilePlayer/ViewController.h
@@ -7,9 +7,28 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <EZAudio/EZAudio.h>
 
 @interface ViewController : UIViewController
 
+//------------------------------------------------------------------------------
+#pragma mark - Properties
+//------------------------------------------------------------------------------
+
+//
+// Use the EZAudioPlayer if you just need to play a simple audio file.
+//
+@property (strong, nonatomic) EZAudioPlayer *audioPlayer;
+
+//------------------------------------------------------------------------------
+#pragma mark - Actions
+//------------------------------------------------------------------------------
+
+//
+// Hook up this action to your button in the interface builder to trigger
+// the play function when you touch the button.
+//
+- (IBAction)play:(id)sender;
 
 @end
 
--- SimpleFilePlayer/SimpleFilePlayer/ViewController.m
+++ SimpleFilePlayer/SimpleFilePlayer/ViewController.m
@@ -8,20 +8,47 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
-@end
-
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //
+    // Get the audio file as an NSURL from the NSBundle, which contains the
+    // media resources you drop into any Xcode project. Look left
+    // at the project contents and you'll see I've dragged an audio file
+    // into the project named "audio_file.wav". You can change this to
+    // any other audio file by dragging in your audio file and changing the
+    // name and extension of the resource below.
+    //
+    NSURL *fileURL = [[NSBundle mainBundle] URLForResource:@"audio_file" withExtension:@"wav"];
+    
+    //
+    // Setup the audio player using that NSURL instance.
+    //
+    self.audioPlayer = [EZAudioPlayer audioPlayerWithURL:fileURL];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//------------------------------------------------------------------------------
+#pragma mark - Actions
+//------------------------------------------------------------------------------
+
+- (void)play:(id)sender {
+    //
+    // Check if the audio player is currently playing.
+    //
+    if ([self.audioPlayer isPlaying]) {
+        //
+        // Pause the audio player if it is currently playing.
+        //
+        [self.audioPlayer pause];
+    }
+    else {
+        //
+        // Play if the audio player is not currently playing.
+        //
+        [self.audioPlayer play];
+    }
 }
 
 @end
